5c9517e73c32c10c7c0be7745b3d2fde103cad7e
Timothy Lin <timothy0336@hotmail.com>
2024-12-21 15:08:38 +0800

    add traits

---
+++ traits/src/lib.rs
@@ -0,0 +1,85 @@
+pub mod lifetime;
+use std::fmt::Display;
+
+pub trait Summary {
+    fn summarize(&self) -> String;
+}
+
+pub trait SummaryDefault {
+    fn summarize_author(&self) -> String;
+
+    // Default implementation
+    fn summarize2(&self) -> String {
+        format!("(Read more from {}...)", self.summarize_author())
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
+
+impl SummaryDefault for Tweet {
+    fn summarize_author(&self) -> String {
+        format!("@{}", self.username)
+    }
+}
+
+// Longer form - pub f
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+// Using Trait Bounds to Conditionally Implement Methods
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
+
+fn main() {
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize2());
+}
---
+++ traits/src/lifetime.rs
@@ -0,0 +1,53 @@
+use std::fmt::Display;
+
+pub fn run_lifetime() {
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
+
+    let result = longest(string1.as_str(), string2);
+    println!("The longest string is {result}");
+
+    let val = get_x_or_zero(&10, &20);
+    println!("The value is {}", val);
+
+    let result2 = longest_with_an_announcement(string1.as_str(), string2, "Hello");
+    println!("The longest string is {result2}");
+}
+
+static ZERO: i32 = 0;
+
+pub fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+// If y is not returned, there's no need to specify the lifetime of y
+pub fn get_x_or_zero<'a>(x: &'a i32, y: &i32) -> &'a i32 {
+    if *x > *y {
+        x
+    } else {
+        &ZERO
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {ann}");
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
+
+struct Foo<'a> {
+    bar: &'a i32,
+}
+fn baz<'a>(f: &Foo<'a>) -> &'a i32 {
+    f.bar
+}
